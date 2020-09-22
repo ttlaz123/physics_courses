65516de4e60b4bd6f8a9f4b50c74db71a481e115
ttlaz123 <tongtianliu@gmail.com>
2020-09-22 11:44:20 -0700

    Create random_walk.py

---
+++ random_walk.py
@@ -0,0 +1,71 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np 
+import random
+
+def walk_path(start, n, d=2):
+    assert(len(start) == d)
+    path = [start]
+    for i in range(n):
+        current_pos = path[i][:]
+        next_pos = choose_dir(current_pos, d)
+        path.append(next_pos)
+    return path
+
+def draw_paths(paths, num_steps):
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    rootn = np.sqrt(num_steps)
+    scale = rootn*1.5
+    circle = plt.Circle((0,0), rootn, color='lavender')
+    ax.add_artist(circle)
+    for path in paths:
+        path = np.array(path)
+        ax.plot(*path.T)
+    
+    plt.xlim(-scale, scale)
+    plt.ylim(-scale, scale)
+    plt.title("Number of steps: " + str(num_steps) + ' , sqrt(N)=' + str(rootn))
+    plt.show()
+    return
+def plot_ends(paths, num_steps):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    rootn = np.sqrt(num_steps)
+    scale = rootn*2
+    circle = plt.Circle((0,0), rootn, color='lavender', fill=False)
+    ax.add_artist(circle)
+    endpointsx =[]
+    endpointsy=[]
+    for path in paths:
+        endpoint = path[-1]
+        endpointsx.append(endpoint[0])
+        endpointsy.append(endpoint[1])
+    ax.scatter(endpointsx, endpointsy)
+    plt.xlim(-scale, scale)
+    plt.ylim(-scale, scale)
+    plt.title("Number of steps: " + str(num_steps) + ' , sqrt(N)=' + str(rootn))
+    plt.show()
+    return
+
+def choose_dir(pos, d):
+    ind = random.choice(range(d))
+    sign = random.randint(0,1)
+    if(sign == 0):
+        sign = -1
+    pos[ind] += sign
+    return pos
+
+def main():
+    num_paths = 1000
+    num_steps = 50
+    start = [0,0]
+    paths = []
+    for i in range(num_paths):
+        path = walk_path(start, num_steps)
+        paths.append(path)
+    #draw_paths(paths, num_steps)
+    plot_ends(paths, num_steps)
+if __name__ == '__main__':
+    main()
