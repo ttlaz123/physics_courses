1a716eb4c0673ad91f6eeb7ccc7182809fd834f6
ttlaz123 <tongtianliu@gmail.com>
2020-09-23 11:52:31 -0700

    finished problem sets

--- cos_integrals.py
+++ cos_integrals.py
@@ -1,1 +1,132 @@
-import scipy
+
+from scipy import integrate
+import numpy as np
+import argparse
+
+
+def f4(w, x, y, z):
+    inner = np.cos(w) + np.cos(x) + np.cos(y) + np.cos(z)
+    total = (inner / 4)
+    return f4
+
+
+def boundsz():
+    return [-np.pi, np.pi]
+
+
+def boundsy(z):
+    return [-np.pi, np.pi]
+
+
+def boundsx(y, z):
+    return [-np.pi, np.pi]
+
+
+def boundsw(x, y, z):
+    return [-np.pi, np.pi]
+
+
+def f4(w, x, y, z, n):
+    temp = (np.cos(w)+np.cos(x) + np.cos(y) + np.cos(z)) / 4
+    numer = np.power(temp, n)
+    denom = np.power((2*np.pi), 4)
+    return numer / denom
+
+
+def f3(x, y, z, n):
+    temp = (np.cos(x) + np.cos(y) + np.cos(z)) / 3
+    numer = np.power(temp, n)
+    denom = np.power((2*np.pi), 3)
+    return numer / denom
+
+
+def f2(y, z, n):
+    temp = (np.cos(y) + np.cos(z)) / 2
+    numer = np.power(temp, n)
+    denom = np.power((2*np.pi), 2)
+    return numer/denom
+
+
+def f1(z, n):
+    temp = (np.cos(z)) / 1
+    numer = np.power(temp, n)
+    denom = np.power((2*np.pi), 1)
+    return numer/denom
+
+
+def bounds_z(n):
+
+    return [-np.pi, np.pi]
+
+
+def bounds_y(z, n):
+
+    return [-np.pi, np.pi]
+
+
+def bounds_x(y, z, n):
+
+    return [-np.pi, np.pi]
+
+
+def bounds_w(x, y, z, n):
+
+    return [-np.pi, np.pi]
+
+
+def sum_4(N=20):
+    sum = 0
+    for n in range(N):
+        if (n % 2 == 1):
+            continue
+        result = integrate.nquad(
+            f4, [bounds_w, bounds_x, bounds_y, bounds_z], args=(n,))
+        print(result)
+        sum += result[0]
+    print(sum)
+
+
+def sum_3(N=20):
+    sum = 0
+    for n in range(N):
+        if (n % 2 == 1):
+            continue
+        result = integrate.nquad(f3, [bounds_x, bounds_y, bounds_z], args=(n,))
+        print(result)
+        print(sum)
+        sum += result[0]
+    print(sum)
+
+
+def sum_2(N=20):
+    sum = 0
+    for n in range(N):
+        result = integrate.nquad(f2, [bounds_y, bounds_z], args=(n,))
+        print(result)
+        sum += result[0]
+    print(sum)
+
+
+def sum_1(N=20):
+    sum = 0
+    for n in range(N):
+        result = integrate.nquad(f1, [bounds_z], args=(n,))
+        print(result)
+        sum += result[0]
+    print(sum)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-N', '--limit', type=int, default=20)
+parser.add_argument('-d', '--dim', type=int)
+args = parser.parse_args()
+if (args.dim == 1):
+    sum_1(args.limit)
+
+if (args.dim == 2):
+    sum_2(args.limit)
+if (args.dim == 3):
+    sum_3(args.limit)
+
+if (args.dim == 4):
+    sum_4(args.limit)
--- random_walk.py
+++ random_walk.py
@@ -1,7 +1,8 @@
 import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 import random
+
 
 def walk_path(start, n, d=2):
     assert(len(start) == d)
@@ -12,60 +13,88 @@ def walk_path(start, n, d=2):
         path.append(next_pos)
     return path
 
+
 def draw_paths(paths, num_steps):
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     rootn = np.sqrt(num_steps)
     scale = rootn*1.5
-    circle = plt.Circle((0,0), rootn, color='lavender')
+    circle = plt.Circle((0, 0), rootn, color='lavender')
     ax.add_artist(circle)
     for path in paths:
         path = np.array(path)
         ax.plot(*path.T)
-    
+
     plt.xlim(-scale, scale)
     plt.ylim(-scale, scale)
-    plt.title("Number of steps: " + str(num_steps) + ' , sqrt(N)=' + str(rootn))
+    plt.title("Number of steps: " + str(num_steps) +
+              ' , sqrt(N)=' + str(rootn))
     plt.show()
     return
+
+
 def plot_ends(paths, num_steps):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+
     rootn = np.sqrt(num_steps)
     scale = rootn*2
-    circle = plt.Circle((0,0), rootn, color='lavender', fill=False)
-    ax.add_artist(circle)
-    endpointsx =[]
-    endpointsy=[]
+
+    endpointsx = []
+    endpointsy = []
     for path in paths:
         endpoint = path[-1]
         endpointsx.append(endpoint[0])
         endpointsy.append(endpoint[1])
+
+    fig = plt.figure()
+    circle = plt.Circle((0, 0), rootn, color='lavender', fill=False)
+    ax = fig.add_subplot(111)
+    ax.add_artist(circle)
+
     ax.scatter(endpointsx, endpointsy)
     plt.xlim(-scale, scale)
     plt.ylim(-scale, scale)
-    plt.title("Number of steps: " + str(num_steps) + ' , sqrt(N)=' + str(rootn))
+    plt.title("Number of steps: " + str(num_steps) +
+              ' , sqrt(N)=' + str(rootn))
     plt.show()
+    plot_angles(endpointsx, endpointsy)
     return
+
 
 def choose_dir(pos, d):
     ind = random.choice(range(d))
-    sign = random.randint(0,1)
+    sign = random.randint(0, 1)
     if(sign == 0):
         sign = -1
     pos[ind] += sign
     return pos
 
+
+def plot_angles(endx, endy):
+
+    angles = np.arctan2(endy, endx)
+    print(angles)
+    fig = plt.figure()
+    print('making hist')
+    plt.hist(angles, bins=30)
+    print('done')
+    plt.title('Histogram of endpoint angles')
+    plt.xlabel('Angle(radians)')
+    plt.ylabel('Count')
+    plt.show()
+
+
 def main():
     num_paths = 1000
-    num_steps = 50
-    start = [0,0]
+    num_steps = 10000
+    start = [0, 0]
     paths = []
     for i in range(num_paths):
         path = walk_path(start, num_steps)
         paths.append(path)
     #draw_paths(paths, num_steps)
     plot_ends(paths, num_steps)
+
+
 if __name__ == '__main__':
     main()
